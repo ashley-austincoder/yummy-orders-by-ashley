a61e720663d6c497f5758e6c6c46614856160205
Ashley Smith <ashleymarie4989@gmail.com>
2022-12-14 19:10:17 -0800

    Add React and Node templates

---
+++ spec/config.rb
@@ -0,0 +1,12 @@
+# Modify these as needed
+
+HTTP_SUCCESS = 200
+HTTP_BAD_REQUEST = 400
+HTTP_NOT_FOUND = 404
+HTTP_UNPROCESSABLE = 422
+HTTP_ERROR = 500
+BASE_URL = 'http://localhost:3000'
+RESET_DB_COMMAND = 'mysql -u root -p yummy_development < yummy_development.sql.dump'
+
+USER_ONE_ID = 1
+USER_TWO_ID = 2
